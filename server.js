592fadd0ca72ac888450bf53a4095bf1d2128ae9
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-07-24 16:26:59 +0200

    adding css

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const aboutFolderPath = path.join(clientsFolderPath, 'About');
 const blogFolderPath = path.join(clientsFolderPath, 'Blog');
 const contactFolderPath = path.join(clientsFolderPath, 'Contact');
 
+// Create subfolders inside Clients folder
+const subfolders = ['About', 'Contact', 'Blog'];
+subfolders.forEach(subfolder => {
+  const subfolderPath = path.join(clientsFolderPath, subfolder);
+  if (!fs.existsSync(subfolderPath)) {
+    fs.mkdirSync(subfolderPath);
+    console.log(`"${subfolder}" folder created...`);
+  }
+});
+
 // Create and write to file inside Clients folder
 fs.writeFile(
   path.join(clientsFolderPath, 'index.html'),
@@ -15,6 +25,15 @@ fs.writeFile(
     console.log('File written to...');
   }
 );
+
+fs.writeFile(
+    path.join(clientsFolderPath, 'style.css'),
+    'body { background-color:#FF00FF; color: #333; font-family: Arial, sans-serif; }',
+    err => {
+      if (err) throw err;
+      console.log('CSS file written...');
+    }
+  );
 
 // Create and write to file inside About folder
 fs.writeFile(
@@ -26,7 +45,16 @@ fs.writeFile(
       }
 );
 
-// Create and write to file inside About folder
+fs.writeFile(
+    path.join(aboutFolderPath, 'style.css'),
+    'body { background-color:#FFC0CB; color: #333; font-family: Arial, sans-serif; }',
+    err => {
+      if (err) throw err;
+      console.log('CSS file written...');
+    }
+  );
+
+// Create and write to file inside Blog folder
 fs.writeFile(
     path.join(blogFolderPath, 'index.html'),
     'Hi Barbie',
@@ -36,7 +64,16 @@ fs.writeFile(
       }
 );
 
-// Create and write to file inside About folder
+fs.writeFile(
+    path.join(blogFolderPath, 'style.css'),
+    'body { background-color:#DA70D6; color: #333; font-family: Arial, sans-serif; }',
+    err => {
+      if (err) throw err;
+      console.log('CSS file written...');
+    }
+  );
+
+// Create and write to file inside Contact folder
 fs.writeFile(
     path.join(contactFolderPath, 'index.html'),
     'Hi Barbie',
@@ -46,12 +83,13 @@ fs.writeFile(
       }
 );
 
-// Create subfolders inside Clients folder
-const subfolders = ['About', 'Contact', 'Blog'];
-subfolders.forEach(subfolder => {
-  const subfolderPath = path.join(clientsFolderPath, subfolder);
-  if (!fs.existsSync(subfolderPath)) {
-    fs.mkdirSync(subfolderPath);
-    console.log(`"${subfolder}" folder created...`);
-  }
-});
+fs.writeFile(
+    path.join(contactFolderPath, 'style.css'),
+    'body { background-color:#FF69B4; color: #333; font-family: Arial, sans-serif; }',
+    err => {
+      if (err) throw err;
+      console.log('CSS file written...');
+    }
+  );
+
+
