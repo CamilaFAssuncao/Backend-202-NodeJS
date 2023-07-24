181be7685f629dbfabe05bb85ca79763b66b9c76
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-07-24 16:14:31 +0200

    adding files

--- server.js
+++ server.js
@@ -1,13 +1,57 @@
-
 const path = require('path');
 const fs = require('fs');
 
-// Create and write to file
+const clientsFolderPath = path.join(__dirname, 'Clients');
+const aboutFolderPath = path.join(clientsFolderPath, 'About');
+const blogFolderPath = path.join(clientsFolderPath, 'Blog');
+const contactFolderPath = path.join(clientsFolderPath, 'Contact');
+
+// Create and write to file inside Clients folder
 fs.writeFile(
-    path.join(__dirname, 'Clients', 'index.html'), // Corrected path with 'Clients' folder and 'index.html' file
-    'Hello World!', // Content to be written in the file
+  path.join(clientsFolderPath, 'index.html'),
+  'Hello World!',
+  err => {
+    if (err) throw err;
+    console.log('File written to...');
+  }
+);
+
+// Create and write to file inside About folder
+fs.writeFile(
+    path.join(aboutFolderPath, 'index.html'),
+    'Hello hello',
     err => {
         if (err) throw err;
         console.log('File written to...');
-    }
+      }
 );
+
+// Create and write to file inside About folder
+fs.writeFile(
+    path.join(blogFolderPath, 'index.html'),
+    'Hi Barbie',
+    err => {
+        if (err) throw err;
+        console.log('File written to...');
+      }
+);
+
+// Create and write to file inside About folder
+fs.writeFile(
+    path.join(contactFolderPath, 'index.html'),
+    'Hi Barbie',
+    err => {
+        if (err) throw err;
+        console.log('File written to...');
+      }
+);
+
+// Create subfolders inside Clients folder
+const subfolders = ['About', 'Contact', 'Blog'];
+subfolders.forEach(subfolder => {
+  const subfolderPath = path.join(clientsFolderPath, subfolder);
+  if (!fs.existsSync(subfolderPath)) {
+    fs.mkdirSync(subfolderPath);
+    console.log(`"${subfolder}" folder created...`);
+  }
+});
