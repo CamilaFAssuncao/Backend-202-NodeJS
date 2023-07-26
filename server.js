94778678ad576a84380dd06c8101e9e56c299dad
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-07-26 10:16:39 +0200

    routing

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const fs = require('fs');
 const http = require('http');
 const portfinder = require('portfinder');
 
+
 const clientsFolderPath = path.join(__dirname, 'Clients');
 const subfolders = [
   { name: 'About', color: '#FFC0CB' },
   { name: 'Contact', color: '#FF69B4' },
   { name: 'Blog', color: '#DA70D6' }
 ];
+
 
 // Function to create a folder and write to files inside it
 function createFolderAndFiles(folderPath, pageTitle, cssContent) {
@@ -36,7 +38,9 @@ function createFolderAndFiles(folderPath, pageTitle, cssContent) {
     <html>
     <head>
       <title>${pageTitle}</title>
-      <link rel="stylesheet" href="./style.css">
+      <style>
+        body { background-color:${subfolder.color}; color: #333; font-family: Arial, sans-serif; }
+      </style>
     </head>
     <body>
       <h1>${pageTitle}</h1>
@@ -75,12 +79,57 @@ portfinder.getPort((err, port) => {
   if (err) {
     console.error('Error finding available port:', err);
   } else {
-    // Start the server only once
-    const server = http.createServer();
-    server.on('request', (req, res) => {
-      console.log('A request has been submitted');
+
+
+    const server = http.createServer((req, res) => {
+        res.setHeader('Content-Type', 'text/html');
+        console.log(`${req.method} ${req.url}`);
+      
+        const extension = path.extname(req.url).slice(1);
+        const type = extension ? types[extension] : types.html;
+        const supportedExtension = Boolean(type);
+      
+        if (!supportedExtension) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end('404: File not found');
+          return;
+        }
+
+         // routing
+  let route = './Clients/';
+  switch(req.url) {
+    case '/':
+      route += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/About':
+      route += 'index.html';
+      res.statusCode = 200;
+      break;
+      case '/Blog':
+      route += 'index.html';
+      res.statusCode = 200;
+      break;
+      case '/Contact':
+      route += 'index.html';
+      res.statusCode = 200;
+      break;
+    default:
+      route += 'index.html';
+      res.statusCode = 404;
+  }
+
+  // send html
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
       res.end();
-    });
+    }
+    //res.write(data);
+    res.end(data);
+  });
+      });
+      
 
     server.listen(port, () => {
       console.log(`Server started on http://127.0.0.1:${port}`);
